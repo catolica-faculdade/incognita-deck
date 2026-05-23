239fdc12c98d85cc1682c86b0d15861690b0447a
gfloriano11 <gflorianodev@gmail.com>
2026-05-23 20:29:03 -0300

    feat: add scene and script to line and trajectory

---
+++ world/player/trajectory.gd
@@ -0,0 +1,21 @@
+extends Line2D
+
+@export var grid_unity: float = 75.0
+
+func _ready():
+	clear_points()
+	add_point(Vector2.ZERO)
+
+func update_straight_line(x_position: float, y_position: float):
+	clear_points()
+	
+	if x_position == 0 and y_position == 0:
+		return
+	
+	add_point(Vector2.ZERO)
+	
+	var final_x_position = x_position * grid_unity
+	var final_y_position = y_position * grid_unity * -1
+	
+	var final_position = Vector2(final_x_position, final_y_position)
+	add_point(final_position)
